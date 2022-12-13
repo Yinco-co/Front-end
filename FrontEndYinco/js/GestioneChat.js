a25775bf67399a967697df6ba11a8879c175610e
Daniele Notarangelo <50075274+danielenotarangelo@users.noreply.github.com>
2022-12-13 09:49:12 +0100

    Add files via upload

---
+++ FrontEndYinco/js/GestioneChat.js
@@ -0,0 +1,81 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = document.getElementById("testo");
+const msgerChat = get(".messaggi");
+
+const BOT_MSGS = [
+  "Hi, how are you?",
+  "Ohh... I can't understand what you trying to say. Sorry!",
+  "I like to play games... But I don't know how to play!",
+  "Sorry if my answers are not relevant. :))",
+  "I feel sleepy! :("
+];
+
+const BOT_IMG = "Img/Logo.png";
+const BOT_NAME = "YINCO";
+const PERSON_NAME = "Studente";
+
+msgerForm.addEventListener("click", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessageUser(PERSON_NAME, "right", msgText);
+  msgerInput.value = "";
+
+  botResponse();
+});
+
+function appendMessageUser(name, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function appendMessageBot(name, img, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse() {
+  const r = random(0, BOT_MSGS.length - 1);
+  const msgText = BOT_MSGS[r];
+  const delay = msgText.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessageBot(BOT_NAME, BOT_IMG, "left", msgText);
+  }, delay);
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
